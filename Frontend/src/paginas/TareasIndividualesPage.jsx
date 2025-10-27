0e89b4f167799d7f8f6242b67e6eae018981ae53
Gerardo <gerarleonce-0703@outlook.es>
2025-10-27 11:55:39 -0500

    Cambio para colocar el botón de invitar alumno dentro del contenedor y a la izquierda

--- Frontend/src/paginas/TareasIndividualesPage.jsx
+++ Frontend/src/paginas/TareasIndividualesPage.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import TareasIndividuales from "../componentes/TareasIndividuales";
-import InvitacionButton from "../componentes/InvitacionButton";
 import AsignarNotas from "./AsignarNotas";
 
 export default function TareasIndividualesPage() {
   return (
     <div>
-      <InvitacionButton />
       <h2>Tareas Individuales</h2>
       <TareasIndividuales />
     </div>
